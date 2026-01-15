4cff1dcb609e1e66912f85dd429fc87db2b2327a
Gayathri <gayathrimh11@gmail.com>
2026-01-15 15:37:39 +0530

    adding calendar-app

---
+++ Day2/calendar-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Page1 from './pages/Page1.jsx' 
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+       <h1> Calendar 2026</h1>
+        <Page1 />
+    </div>
+  );
+}
+
+export default App;
