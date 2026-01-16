2c07142bd8fe826ca86a738384f4c0d41bfc2818
Gayathri <gayathrimh11@gmail.com>
2026-01-16 13:59:27 +0530

    countries-app

--- Day2/calendar-app/src/pages/Page1.jsx
+++ Day2/calendar-app/src/pages/Page1.jsx
@@ -7,7 +7,6 @@ const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-
 const currentDate = new Date();
 const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
 const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
